7f2ffc491ac6635f231f806ef241d1595da0bf92
sofanisba <stefanov.tamara@gmail.com>
2017-02-13 18:58:29 -0500

    init

---
+++ pretty_print.rb
@@ -0,0 +1,17 @@
+require 'json'
+require 'pry'
+
+puts "input filename"
+start = gets.chomp
+puts "output filename"
+finish = gets.chomp
+
+begin
+  input = File.read(start)
+  json = JSON.parse(measures)
+
+  output = File.open(finish, 'w')
+  output.write(JSON.pretty_generate(json))
+ensure
+  output.close unless output.nil?
+end
